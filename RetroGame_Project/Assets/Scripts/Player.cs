93d4e6a9448138cf33daa291381d96befc312972
Luca <contact.lucanaselli@gmail.com>
2020-11-18 15:47:23 +0100

    changed theme from kitchen to magic wizards

---
+++ RetroGame_Project/Assets/Scripts/Player.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [ Header( "Movement" ) ]
+    [ SerializeField ] private float speed;
+    [ SerializeField ] private string player = "1";
+    
+    private Vector2     _moveVector;
+    private Rigidbody2D _rb;
+
+    [ Header( "current state" ) ]
+    [ SerializeField ] private GameObject    itemIsHolding;
+    [ SerializeField ] private GameObject     itemYouAreOnTopOf;
+    [ SerializeField ] private Kettle currentLocation;
+
+    private void Start(){
+        _rb = GetComponent< Rigidbody2D >();
+    }
+
+    private void Update(){
+        _moveVector.x = Input.GetAxisRaw( "Horizontal" + player );
+        _moveVector.y = Input.GetAxisRaw( "Horizontal" + player );
+    }
+
+    private void FixedUpdate(){
+        _rb.MovePosition(_rb.position + _moveVector * (speed * Time.fixedDeltaTime));
+    }
+    
+    private void OnTriggerEnter2D( Collider2D other ){
+        if (other.TryGetComponent<Potion>(out var potion))
+            itemYouAreOnTopOf = potion.gameObject;
+        else if (other.TryGetComponent<Ingredient>(out var ingredient))
+            itemYouAreOnTopOf = ingredient.gameObject;
+        else if (other.TryGetComponent<Kettle>(out var kettle))
+            currentLocation = kettle;
+        
+    }
+    private void OnTriggerExit2D( Collider2D other ){
+        //if (other.GetComponent<Item>() == itemYouAreOnTopOf)
+            itemYouAreOnTopOf = null;
+        if (other.GetComponent<Kettle>() == currentLocation)
+            currentLocation = null;
+        
+    }
+}
